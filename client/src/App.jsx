f7f9f8f6bad0418207dcc223dfb79070a7753802
OUKHATOU HAMZA <hamzaoukhatou@gmail.com>
2025-04-25 23:13:17 +0100

    Add signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/NavBar'
 import Footer from './components/Footer'
 import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow pt-16 pb-20"> {/* Add padding for fixed navbar and footer */}
+        <main className="flex-grow pt-16 pb-20"> 
           <Routes>
             <Route path="/" element={<div className="p-4"></div>} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<div className="p-4"></div>} />
+            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </main>
         <Footer />
